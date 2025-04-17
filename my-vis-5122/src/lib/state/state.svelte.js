b7d5633ec950f73c4e4292300033565ebedb1d5c
Zhaocong Yang <zyang19@uncc.edu>
2025-04-17 13:54:57 -0400

    modify size

--- my-vis-5122/src/lib/state/state.svelte.js
+++ my-vis-5122/src/lib/state/state.svelte.js
@@ -1,12 +1,12 @@
 export const config = $state({
     barChart: {
-        containerWidth: 260,
-        containerHeight: 300,
+        containerWidth: 360,
+        containerHeight: 500,
         margin: {top: 25, right: 20, bottom: 20, left: 40},
     },
     scatterPlot: {
-        containerWidth: 500,
-        containerHeight: 300,
+        containerWidth: 800,
+        containerHeight: 500,
         margin: {top: 25, right: 20, bottom: 20, left: 35},
         tooltipPadding: 15,
     }
